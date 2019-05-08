70458d28c6fa401b0815082a6ac981e2cd058515
spreka <42864288+spreka@users.noreply.github.com>
2019-05-08 09:41:48 +0200

    allow many style test images

--- biomag-kaggle/src/2_DL/style-transfer/pytorch-CycleGAN-and-pix2pix-etasnadi/options/test_options.py
+++ biomag-kaggle/src/2_DL/style-transfer/pytorch-CycleGAN-and-pix2pix-etasnadi/options/test_options.py
@@ -11,5 +11,5 @@ class TestOptions(BaseOptions):
         self.parser.add_argument('--aspect_ratio', type=float, default=1.0, help='aspect ratio of result images')
         self.parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
         self.parser.add_argument('--which_epoch', type=str, default='latest', help='which epoch to load? set to latest to use latest cached model')
-        self.parser.add_argument('--how_many', type=int, default=50, help='how many test images to run')
+        self.parser.add_argument('--how_many', type=int, default=1000, help='how many test images to run') #50
         self.isTrain = False
